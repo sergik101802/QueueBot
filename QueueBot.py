a2e10b8024506c7569102b5d10bd6138c46dfc77
sergik101802 <94650398+sergik101802@users.noreply.github.com>
2022-11-01 16:06:58 +0200

    Update QueueBot.py

--- QueueBot.py
+++ QueueBot.py
@@ -1,7 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, ContextTypes, \
+    CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 import logging
-
 
 # посилання на бота: https://t.me/QueueTM_bot
 # https://core.telegram.org/bots/api Telegram Bot API
@@ -13,6 +13,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 NAME = range(1)
+
+buttons = [  # створюємо кнопочки, на які будемо нажимать (циклом не виходило ініціалізувать buttons,
+    # нічого просто потім не надсилається)
+    [InlineKeyboardButton("1", callback_data="1"), InlineKeyboardButton("2", callback_data="2"),
+     InlineKeyboardButton("3", callback_data="3"), InlineKeyboardButton("4", callback_data="4")],
+    [InlineKeyboardButton("5", callback_data="5"), InlineKeyboardButton("6", callback_data="6"),
+     InlineKeyboardButton("7", callback_data="7"), InlineKeyboardButton("8", callback_data="8")],
+    [InlineKeyboardButton("9", callback_data="9"), InlineKeyboardButton("10", callback_data="10"),
+     InlineKeyboardButton("11", callback_data="11"), InlineKeyboardButton("12", callback_data="12")],
+    [InlineKeyboardButton("13", callback_data="13"), InlineKeyboardButton("14", callback_data="14"),
+     InlineKeyboardButton("15", callback_data="15"), InlineKeyboardButton("16", callback_data="16")],
+    [InlineKeyboardButton("17", callback_data="17"), InlineKeyboardButton("18", callback_data="18"),
+     InlineKeyboardButton("19", callback_data="19"), InlineKeyboardButton("20", callback_data="20")],
+    [InlineKeyboardButton("21", callback_data="21"), InlineKeyboardButton("22", callback_data="22"),
+     InlineKeyboardButton("23", callback_data="23"), InlineKeyboardButton("24", callback_data="24")],
+    [InlineKeyboardButton("25", callback_data="25"), InlineKeyboardButton("26", callback_data="26"),
+     InlineKeyboardButton("27", callback_data="27"), InlineKeyboardButton("28", callback_data="28")],
+    [InlineKeyboardButton("29", callback_data="29"), InlineKeyboardButton("30", callback_data="30")]
+]
+# https://core.telegram.org/bots/api#inlinekeyboardmarkup
+
+keyboard = InlineKeyboardMarkup(buttons)  # засовуємо наші кнопочки в єдину клавіатуру
 
 
 # опис команди /start
@@ -37,29 +59,9 @@ def namequeue(update, context):  # вивід черги
     for i in range(1, 31):
         queue += str(i) + '.\n'  # створюємо список (1.\n2.\n)
 
-    buttons = [  # створюємо кнопочки, на які будемо нажимать (циклом не виходило ініціалізувать buttons,
-        # нічого просто потім не надсилається)
-        [InlineKeyboardButton("1", callback_data="1"), InlineKeyboardButton("2", callback_data="2"),
-         InlineKeyboardButton("3", callback_data="3"), InlineKeyboardButton("4", callback_data="4")],
-        [InlineKeyboardButton("5", callback_data="5"), InlineKeyboardButton("6", callback_data="6"),
-         InlineKeyboardButton("7", callback_data="7"), InlineKeyboardButton("8", callback_data="8")],
-        [InlineKeyboardButton("9", callback_data="9"), InlineKeyboardButton("10", callback_data="10"),
-         InlineKeyboardButton("11", callback_data="11"), InlineKeyboardButton("12", callback_data="12")],
-        [InlineKeyboardButton("13", callback_data="13"), InlineKeyboardButton("14", callback_data="14"),
-         InlineKeyboardButton("15", callback_data="15"), InlineKeyboardButton("16", callback_data="16")],
-        [InlineKeyboardButton("17", callback_data="17"), InlineKeyboardButton("18", callback_data="18"),
-         InlineKeyboardButton("19", callback_data="19"), InlineKeyboardButton("20", callback_data="20")],
-        [InlineKeyboardButton("21", callback_data="21"), InlineKeyboardButton("22", callback_data="22"),
-         InlineKeyboardButton("23", callback_data="23"), InlineKeyboardButton("24", callback_data="24")],
-        [InlineKeyboardButton("25", callback_data="25"), InlineKeyboardButton("26", callback_data="26"),
-         InlineKeyboardButton("27", callback_data="27"), InlineKeyboardButton("28", callback_data="28")],
-        [InlineKeyboardButton("29", callback_data="29"), InlineKeyboardButton("30", callback_data="30")]
-    ]
-    # https://core.telegram.org/bots/api#inlinekeyboardmarkup
-
-    keyboard = InlineKeyboardMarkup(buttons)  # засовуємо наші кнопочки в єдину клавіатуру
     update.message.reply_text(name + queue, reply_markup=keyboard)  # надсилаємо повідомлення: назва черги, список та
     # клавіатуру
+    # update.message.reply_text(update.message.from_user.first_name+' '+update.message.from_user.last_name)
     return ConversationHandler.END  # повертаємо кінець діалогу
 
 
